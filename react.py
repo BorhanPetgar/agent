e443afe0f48b117f53f858b596a87f64700d2968
BorhanPetgar <borhanpetgar40@gmail.com>
2025-02-21 20:05:51 +0000

    Add early stop and more question

--- react.py
+++ react.py
@@ -3,7 +3,6 @@ from prompt import prompt
 from tools import known_actions, calculate, wikipedia
 import re
 
-
 action_re = re.compile('^Action: (\w+): (.*)')
 
 def query(question, max_turns=5):
@@ -15,18 +14,35 @@ def query(question, max_turns=5):
         i += 1
         result = bot(next_prompt)
         print(result)
-        actions = [action_re.match(a) for a in result.split('\n') if action_re.match(a)]
-        if actions:
-            action, action_input = actions[0].groups()
+        
+        action_match = None
+        for line in result.split('\n'):
+            action_match = action_re.match(line)
+            if action_match:
+                break
+        
+        if action_match:
+            action, action_input = action_match.groups()
             if action not in known_actions:
                 raise Exception(f'Unknown action: {action}: {action_input}')
+            
             print(f'--- running {action} on {action_input}')
-            observation = known_actions[action](action_input)
-            print('Observation: ', observation)
-            next_prompt = f'Observation: {observation}'
+            try:
+                observation = known_actions[action](action_input)
+                print('Observation: ', observation)
+                next_prompt = f'Observation: {observation}'
+            except Exception as e:
+                print(f'Action failed: {e}')
+                next_prompt = f'Observation: Action failed: {e}'
+                
         else:
             return result
                 
+        if "Answer:" in result:
+            return result
+
                 
                 
-print(query("what is a cat?"))
+print(query("What are the neighbors of Iran?"))
+print(f'******** Question2 ********')
+print(query("What is 5 to the power of 3?"))
