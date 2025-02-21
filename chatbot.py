f1d5e1eab7bbac7ef2cd2a1a725f3b9dd02e32cf
BorhanPetgar <borhanpetgar40@gmail.com>
2025-02-21 22:31:58 +0330

    Add ollama capabality

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,31 @@
 import os
-import openai
 import httpx
+from langchain_community.llms import Ollama
 
+"""
+from langchain_community.llms import Ollama
 
+llm = Ollama(model="llama2")
+
+llm.invoke("tell me about partial functions in python")
+"""
 class ChatBot:
     def __init__(self, system=''):
         self.system = system
-        self.message = []
+        self.messages = []
+        self.llm = Ollama(model="llama2")
+
         if self.system:
             self.messages.append({'role': 'system', 'content': system})
 
     def __call__(self, message):
         self.messages.append({'role': 'user', 'content': message})
-        result = self.exectute()
+        result = self.execute()
         self.messages.append({'role': 'assistant', 'content': result})
         return result
     
     def execute(self):
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
-        return completion.choices[0].message.content
+        result = self.llm.invoke(self.messages)
+        # completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
+        return result
     
